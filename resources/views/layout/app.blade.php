0a6e18c3dbedf0a99c1b23ea4a98cf9c010b9879
Lucas Fell <lucasafell@gmail.com>
2021-01-26 00:48:01 -0300

    sidebar

---
+++ resources/views/layout/app.blade.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+  <title>PluTool</title>
+</head>
+<body>
+  <div class="flex-auto text-white flex-col bg-theme-primary h-screen md:w-48">
+    <div class="flex h-12 w-full">
+      <div class="flex-auto">
+        <p class="h-full text-center">Ic</p>
+      </div>
+      <div class="flex-auto">
+        <p class="h-full text-center">Planning</p>
+      </div>
+      <div class="flex-auto">
+        <p class="h-full text-center">\/</p>
+      </div>
+    </div>
+    <div class="flex-auto bg-theme-contrast h-8 w-full"><p>Design</p></div>
+    <div class="flex-auto h-8 w-full"><p>Configuration</p></div>
+    <div class="flex-auto h-8 w-full"><p>Execution</p></div>
+    <div class="flex-auto h-8 w-full"><p>Monitoring</p></div>
+    <div class="flex-auto h-8 w-full"><p>Analysis</p></div>
+    <div class="flex-auto h-8 w-full"><p>Reporting</p></div>
+  </div>
+</body>
+</html>
