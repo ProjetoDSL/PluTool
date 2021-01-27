8a11b544f9f17f48d450b7ca01b2915980ef082f
Lucas Fell <lucasafell@gmail.com>
2021-01-27 00:03:01 -0300

    filled in sidebar ptbok data

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -5,13 +5,14 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-    <link href="{{ asset('css/fa-app.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/fa.css') }}" rel="stylesheet">
     <link rel="preconnect" href="https://fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css2?family=Quicksand:wght@300&display=swap" rel="stylesheet">
   </head>
   <body>
     <div class="flex-auto scrollbar-thin scrollbar-thumb-theme-contrast scrollbar-track-theme-secondary overflow-y-auto font-wsans text-white flex-col bg-theme-primary h-screen md:w-60 pt-8">
-      <div id="planningBtn" class="flex items-center h-12 w-full cursor-pointer transition duration-200 ease-in-out hover:bg-theme-hover">
+      {{-- Planning --}}
+      <div class="sidebarActivityExpand flex items-center h-12 w-full cursor-pointer transition duration-200 ease-in-out hover:bg-theme-hover">
         <div class="flex-none w-16">
           <p class="text-right mr-4">
             <i class="fas fa-layer-group"></i>
@@ -26,7 +27,7 @@
           </p>
         </div>
       </div>
-      <div id="planningMenu" class="hidden">
+      <div class="hidden">
         <button class="flex items-center h-12 w-full text-sm cursor-pointer bg-theme-secondary focus:outline-none transition duration-200 ease-in-out hover:bg-theme-hover focus:bg-theme-contrast">
           <div class="flex-none w-16">
             <p class="text-right mr-4">
@@ -128,7 +129,76 @@
           </div>
         </button>
       </div>
-      <div class="flex items-center h-12 w-full">
+      {{-- Design --}}
+      <div class="sidebarActivityExpand flex items-center h-12 w-full cursor-pointer transition duration-200 ease-in-out hover:bg-theme-hover">
+        <div class="flex-none w-16">
+        <p class="text-right mr-4">
+          <i class="fas fa-palette"></i>
+        </p>
+        </div>
+        <div class="flex-auto">
+          <p class="text-left">Design</p>
+        </div>
+        <div class="flex-none w-10">
+          <p class="text-center">
+            <i class="fas fa-chevron-right fa-xs"></i>
+          </p>
+        </div>
+      </div>
+      <div class="hidden">
+        <button class="flex items-center h-12 w-full text-sm cursor-pointer bg-theme-secondary focus:outline-none transition duration-200 ease-in-out hover:bg-theme-hover focus:bg-theme-contrast">
+          <div class="flex-none w-16">
+            <p class="text-right mr-4">
+              D5.1
+            </p>
+          </div>
+          <div class="flex-auto">
+            <p class="text-left">Establish Performance Metrics</p>
+          </div>
+        </button>
+        <button class="flex items-center h-12 w-full text-sm cursor-pointer bg-theme-secondary focus:outline-none transition duration-200 ease-in-out hover:bg-theme-hover focus:bg-theme-contrast">
+          <div class="flex-none w-16">
+            <p class="text-right mr-4">
+              D5.2
+            </p>
+          </div>
+          <div class="flex-auto">
+            <p class="text-left">Define Service Level Agreement</p>
+          </div>
+        </button>
+        <button class="flex items-center h-12 w-full text-sm cursor-pointer bg-theme-secondary focus:outline-none transition duration-200 ease-in-out hover:bg-theme-hover focus:bg-theme-contrast">
+          <div class="flex-none w-16">
+            <p class="text-right mr-4">
+              D2.1
+            </p>
+          </div>
+          <div class="flex-auto">
+            <p class="text-left">Define Test Data</p>
+          </div>
+        </button>
+        <button class="flex items-center h-12 w-full text-sm cursor-pointer bg-theme-secondary focus:outline-none transition duration-200 ease-in-out hover:bg-theme-hover focus:bg-theme-contrast">
+          <div class="flex-none w-16">
+            <p class="text-right mr-4">
+              D2.2
+            </p>
+          </div>
+          <div class="flex-auto">
+            <p class="text-left">Implement Test Design</p>
+          </div>
+        </button>
+        <button class="flex items-center h-12 w-full text-sm cursor-pointer bg-theme-secondary focus:outline-none transition duration-200 ease-in-out hover:bg-theme-hover focus:bg-theme-contrast">
+          <div class="flex-none w-16">
+            <p class="text-right mr-4">
+              D3.1
+            </p>
+          </div>
+          <div class="flex-auto">
+            <p class="text-left">Plan and Design Workload</p>
+          </div>
+        </button>
+      </div>
+      {{-- Configuration --}}
+      <div class="sidebarActivityExpand flex items-center h-12 w-full cursor-pointer transition duration-200 ease-in-out hover:bg-theme-hover">
         <div class="flex-none w-16">
         <p class="text-right mr-4">
           <i class="fas fa-cog"></i>
@@ -143,7 +213,30 @@
           </p>
         </div>
       </div>
-      <div class="flex items-center h-12 w-full">
+      <div class="hidden">
+        <button class="flex items-center h-12 w-full text-sm cursor-pointer bg-theme-secondary focus:outline-none transition duration-200 ease-in-out hover:bg-theme-hover focus:bg-theme-contrast">
+          <div class="flex-none w-16">
+            <p class="text-right mr-4">
+              C3.1
+            </p>
+          </div>
+          <div class="flex-auto">
+            <p class="text-left">Configure Ramps</p>
+          </div>
+        </button>
+        <button class="flex items-center h-12 w-full text-sm cursor-pointer bg-theme-secondary focus:outline-none transition duration-200 ease-in-out hover:bg-theme-hover focus:bg-theme-contrast">
+          <div class="flex-none w-16">
+            <p class="text-right mr-4">
+              C4.1
+            </p>
+          </div>
+          <div class="flex-auto">
+            <p class="text-left">Prepare the Test Environment</p>
+          </div>
+        </button>
+      </div>
+      {{-- Execution --}}
+      <div class="sidebarActivityExpand flex items-center h-12 w-full cursor-pointer transition duration-200 ease-in-out hover:bg-theme-hover">
         <div class="flex-none w-16">
         <p class="text-right mr-4">
           <i class="fas fa-play"></i>
@@ -158,7 +251,40 @@
           </p>
         </div>
       </div>
-      <div class="flex items-center h-12 w-full">
+      <div class="hidden">
+        <button class="flex items-center h-12 w-full text-sm cursor-pointer bg-theme-secondary focus:outline-none transition duration-200 ease-in-out hover:bg-theme-hover focus:bg-theme-contrast">
+          <div class="flex-none w-16">
+            <p class="text-right mr-4">
+              E2.1
+            </p>
+          </div>
+          <div class="flex-auto">
+            <p class="text-left">Execute Performance Scenarios</p>
+          </div>
+        </button>
+        <button class="flex items-center h-12 w-full text-sm cursor-pointer bg-theme-secondary focus:outline-none transition duration-200 ease-in-out hover:bg-theme-hover focus:bg-theme-contrast">
+          <div class="flex-none w-16">
+            <p class="text-right mr-4">
+              E6.1
+            </p>
+          </div>
+          <div class="flex-auto">
+            <p class="text-left">Generate Workload</p>
+          </div>
+        </button>
+        <button class="flex items-center h-12 w-full text-sm cursor-pointer bg-theme-secondary focus:outline-none transition duration-200 ease-in-out hover:bg-theme-hover focus:bg-theme-contrast">
+          <div class="flex-none w-16">
+            <p class="text-right mr-4">
+              E7.1
+            </p>
+          </div>
+          <div class="flex-auto">
+            <p class="text-left">Collect Data</p>
+          </div>
+        </button>
+      </div>
+      {{-- Monitoring --}}
+      <div class="sidebarActivityExpand flex items-center h-12 w-full cursor-pointer transition duration-200 ease-in-out hover:bg-theme-hover">
         <div class="flex-none w-16">
         <p class="text-right mr-4">
           <i class="fas fa-desktop"></i>
@@ -173,7 +299,40 @@
           </p>
         </div>
       </div>
-      <div class="flex items-center h-12 w-full">
+      <div class="hidden">
+        <button class="flex items-center h-12 w-full text-sm cursor-pointer bg-theme-secondary focus:outline-none transition duration-200 ease-in-out hover:bg-theme-hover focus:bg-theme-contrast">
+          <div class="flex-none w-16">
+            <p class="text-right mr-4">
+              M2.1
+            </p>
+          </div>
+          <div class="flex-auto">
+            <p class="text-left">Monitor the Test</p>
+          </div>
+        </button>
+        <button class="flex items-center h-12 w-full text-sm cursor-pointer bg-theme-secondary focus:outline-none transition duration-200 ease-in-out hover:bg-theme-hover focus:bg-theme-contrast">
+          <div class="flex-none w-16">
+            <p class="text-right mr-4">
+              M4.1
+            </p>
+          </div>
+          <div class="flex-auto">
+            <p class="text-left">Monitor the Test Environment</p>
+          </div>
+        </button>
+        <button class="flex items-center h-12 w-full text-sm cursor-pointer bg-theme-secondary focus:outline-none transition duration-200 ease-in-out hover:bg-theme-hover focus:bg-theme-contrast">
+          <div class="flex-none w-16">
+            <p class="text-right mr-4">
+              M5.1
+            </p>
+          </div>
+          <div class="flex-auto">
+            <p class="text-left">Monitor the Performance Metrics</p>
+          </div>
+        </button>
+      </div>
+      {{-- Analysis --}}
+      <div class="sidebarActivityExpand flex items-center h-12 w-full cursor-pointer transition duration-200 ease-in-out hover:bg-theme-hover">
         <div class="flex-none w-16">
         <p class="text-right mr-4">
           <i class="fas fa-chart-pie"></i>
@@ -188,7 +347,50 @@
           </p>
         </div>
       </div>
-      <div class="flex items-center h-12 w-full">
+      <div class="hidden">
+        <button class="flex items-center h-12 w-full text-sm cursor-pointer bg-theme-secondary focus:outline-none transition duration-200 ease-in-out hover:bg-theme-hover focus:bg-theme-contrast">
+          <div class="flex-none w-16">
+            <p class="text-right mr-4">
+              A1.1
+            </p>
+          </div>
+          <div class="flex-auto">
+            <p class="text-left">Validate Requirements</p>
+          </div>
+        </button>
+        <button class="flex items-center h-12 w-full text-sm cursor-pointer bg-theme-secondary focus:outline-none transition duration-200 ease-in-out hover:bg-theme-hover focus:bg-theme-contrast">
+          <div class="flex-none w-16">
+            <p class="text-right mr-4">
+              A7.2
+            </p>
+          </div>
+          <div class="flex-auto">
+            <p class="text-left">Analyze Results</p>
+          </div>
+        </button>
+        <button class="flex items-center h-12 w-full text-sm cursor-pointer bg-theme-secondary focus:outline-none transition duration-200 ease-in-out hover:bg-theme-hover focus:bg-theme-contrast">
+          <div class="flex-none w-16">
+            <p class="text-right mr-4">
+              A5.1
+            </p>
+          </div>
+          <div class="flex-auto">
+            <p class="text-left">Validate Service Level Agreement</p>
+          </div>
+        </button>
+        <button class="flex items-center h-12 w-full text-sm cursor-pointer bg-theme-secondary focus:outline-none transition duration-200 ease-in-out hover:bg-theme-hover focus:bg-theme-contrast">
+          <div class="flex-none w-16">
+            <p class="text-right mr-4">
+              A7.1
+            </p>
+          </div>
+          <div class="flex-auto">
+            <p class="text-left">Build Graphs and Charts</p>
+          </div>
+        </button>
+      </div>
+      {{-- Reporting --}}
+      <div class="sidebarActivityExpand flex items-center h-12 w-full cursor-pointer transition duration-200 ease-in-out hover:bg-theme-hover">
         <div class="flex-none w-16">
         <p class="text-right mr-4">
           <i class="fas fa-scroll"></i>
@@ -203,14 +405,26 @@
           </p>
         </div>
       </div>
+      <div class="hidden">
+        <button class="flex items-center h-12 w-full text-sm cursor-pointer bg-theme-secondary focus:outline-none transition duration-200 ease-in-out hover:bg-theme-hover focus:bg-theme-contrast">
+          <div class="flex-none w-16">
+            <p class="text-right mr-4">
+              R7.1
+            </p>
+          </div>
+          <div class="flex-auto">
+            <p class="text-left">Report Results</p>
+          </div>
+        </button>
+      </div>
     </div>
     <script src="{{ asset('js/app.js') }}"></script>
     <script>
       $(function() {
-        $("#planningBtn").on("click", function() {
-          $("#planningBtn > div:last-child > p > i.fa-chevron-right").switchClass('fa-chevron-right', 'fa-chevron-down', 10);
-          $("#planningBtn > div:last-child > p > i.fa-chevron-down").switchClass('fa-chevron-down', 'fa-chevron-right', 0);
-          $("#planningMenu").toggle('blind', {}, 300);
+        $(".sidebarActivityExpand").on("click", function() {
+          $("div:last-child > p > i.fa-chevron-right", this).switchClass('fa-chevron-right', 'fa-chevron-down', 10);
+          $("div:last-child > p > i.fa-chevron-down", this).switchClass('fa-chevron-down', 'fa-chevron-right', 0);
+          $(this).next('div').toggle('blind', {}, 300);
         });
       } );
       </script>
