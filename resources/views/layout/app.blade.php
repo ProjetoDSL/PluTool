91209b77acbe9b4e22a33883f2b10be2d4ba2eff
Lucas Fell <lucasafell@gmail.com>
2021-01-28 03:15:22 -0300

    minor changes

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -14,8 +14,8 @@
       @include('layout.sidebar')
       <div class="flex flex-col w-full">
         @include('layout.timeline')
-        <div class="flex w-full h-full bg-theme-primary p-4">
-          <div class="flex-auto h-full w-full rounded-lg bg-theme-secondary">
+        <div class="flex w-full h-full bg-theme-primary p-2">
+          <div class="flex-auto h-full w-full rounded-lg p-4 bg-theme-secondary">
             @yield('content')
           </div>
         </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -26,6 +26,5 @@ module.exports = {
   ],
   variants: {
     extend: {},
-    scrollbar: ['rounded'],
   },
 }
