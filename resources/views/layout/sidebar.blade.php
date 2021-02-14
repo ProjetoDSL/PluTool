eba355efc3095472e845061f65460d62fe179a08
Lucas Fell <lucasafell@gmail.com>
2021-02-14 15:17:59 -0300

    begin implementing localization

--- resources/views/layout/sidebar.blade.php
+++ resources/views/layout/sidebar.blade.php
@@ -14,40 +14,47 @@
       </div>
     </div>
   </div>
-
-  @foreach ($activities as $phase => $tasks)
-    <div class="sidebarActivityExpand flex items-center h-12 w-full cursor-pointer transition duration-200 ease-in-out hover:bg-theme-hover">
-      <div class="flex-none w-16">
-        <p class="text-right mr-4">
-          <i class="fas fa-{{$icons[$phase]}}"></i>
-        </p>
+  <div>
+    @foreach ($activities as $phase => $tasks)
+      <div class="sidebarActivityExpand flex items-center h-12 w-full cursor-pointer transition duration-200 ease-in-out hover:bg-theme-hover">
+        <div class="flex-none w-16">
+          <p class="text-right mr-4">
+            <i class="fas fa-{{$icons[$phase]}}"></i>
+          </p>
+        </div>
+        <div class="sm:hidden lg:flex flex-auto">
+          <p class="text-left">{{ __('phases.'.$phase) }}</p>
+        </div>
+        <div class="flex-none w-10">
+          <p class="text-center">
+            <i class="fas fa-chevron-right fa-xs"></i>
+          </p>
+        </div>
       </div>
-      <div class="sm:hidden lg:flex flex-auto">
-        <p class="text-left">{{ ucfirst($phase) }}</p>
+      <div class="hidden">
+        @foreach ($tasks as $key => $val)
+          <a href="/{{$phase}}/{{ strtolower(str_replace('.', '-', $key))}}">
+            <button class="flex items-center h-12 w-full text-sm bg-theme-secondary focus:outline-none transition duration-200 ease-in-out hover:bg-theme-hover focus:bg-theme-contrast">
+              <div class="flex-none w-16">
+                <p class="text-right mr-4">{{$key}}</p>
+              </div>
+              <div class="flex-auto">
+                <p class="text-left">{{__('phases.'.$key)}}</p>
+              </div>
+            </button>
+          </a>
+        @endforeach
       </div>
-      <div class="flex-none w-10">
-        <p class="text-center">
-          <i class="fas fa-chevron-right fa-xs"></i>
-        </p>
-      </div>
-    </div>
-    <div class="hidden">
-      @foreach ($tasks as $key => $val)
-        <a href="/{{$phase}}/{{ strtolower(str_replace('.', '-', $key))}}">
-          <button class="flex items-center h-12 w-full text-sm bg-theme-secondary focus:outline-none transition duration-200 ease-in-out hover:bg-theme-hover focus:bg-theme-contrast">
-            <div class="flex-none w-16">
-              <p class="text-right mr-4">
-                {{$key}}
-              </p>
-            </div>
-            <div class="flex-auto">
-              <p class="text-left">{{$val}}</p>
-            </div>
-          </button>
-        </a>
-      @endforeach
-    </div>
-  @endforeach
+    @endforeach
+  </div>
+  <div class="flex justify-around w-full">
+    <button class="w-6">
+      <p> 🇧🇷 </p>
+    </button>
+    <button class="w-6">
+      <p> 🇺🇸 </p>
+    </button>
+  </div>
 </div>
 <script src="{{ asset('js/app.js') }}"></script>
 <script>
