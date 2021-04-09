c95fa19c56bdc1c3bceac3b83fe87b0136c3881a
Lucas Alexandre Fell <47724710+fell-lucas@users.noreply.github.com>
2021-04-09 18:19:40 -0300

    user migration

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,6 +14,7 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call([
+          UserSeeder::class,
           PhaseSeeder::class,
           TaskSeeder::class,
           RequirementsExamplesSeeder::class,
---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeders.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $email = 'plutooladmin@lesse.com.br';
+        $user = DB::table('users')->where('email', '=', $email)->first();
+        if (empty($user)) {
+            DB::table('users')->insert([
+                'name' => 'Administrador',
+                'email' => $email,
+                'password' => Hash::make('123mudar*'),
+                'active' => true
+            ]);
+        }
+    }
+}
