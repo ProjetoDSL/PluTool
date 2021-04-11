af2ddffdffc3fc943087594f8a9bd75ff51fe1af
Lucas <47724710+fell-lucas@users.noreply.github.com>
2021-04-11 13:53:41 -0300

    Revert "Removing sail"

--- deploy.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-tar --exclude ".env" --exclude "public/storage" -czf plutool.tar.gz .
-
-scp -P 65002 ./plutool.tar.gz u648750589@31.170.166.124:~/public_html/public_html/tools/plutool/
-
-ssh -p 65002 u648750589@31.170.166.124 "cd ~/public_html/public_html/tools/plutool/;tar -xvzf plutool.tar.gz;php artisan storage:link"
